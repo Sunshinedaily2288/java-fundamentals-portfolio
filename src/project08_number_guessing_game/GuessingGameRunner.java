511c038af354067dcff92db0107e6ce6db1a14bd
Noorbi Binte Matharsa <sunshinedaily2288@gmail.com>
2026-05-07 22:56:02 +0200

    Project 08:Number Guessing Game with GUI.

---
+++ src/project08_number_guessing_game/GuessingGameRunner.java
@@ -0,0 +1,9 @@
+package project08_number_guessing_game;
+
+public class GuessingGameRunner {
+    public static void main(String[] args) {
+        GuessingGame_OptionPane_GUI game = new GuessingGame_OptionPane_GUI();
+        game.start();
+    }
+}
+
---
+++ src/project08_number_guessing_game/GuessingGame_OptionPane_GUI.java
@@ -0,0 +1,33 @@
+package project08_number_guessing_game;
+
+import javax.swing.JOptionPane;
+
+public class GuessingGame_OptionPane_GUI {
+    private GuessingGameLogic engine = new GuessingGameLogic();
+
+    public void start() {
+        boolean won = false;
+        JOptionPane.showMessageDialog(null, "I'm thinking of a number between 1 and 100!");
+
+        while (!won) {
+            String input = JOptionPane.showInputDialog(null, "Enter your guess:", "Guessing Game", JOptionPane.QUESTION_MESSAGE);
+
+            if (input == null) return; // Exit if user clicks Cancel
+
+            try {
+                int guess = Integer.parseInt(input);
+                String result = engine.processGuess(guess);
+
+                if (result.equals("CORRECT")) {
+                    JOptionPane.showMessageDialog(null, "🎉 Success! You found it in " + engine.getAttempts() + " tries!");
+                    won = true;
+                } else {
+                    JOptionPane.showMessageDialog(null, result);
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Please enter a valid number!");
+            }
+        }
+    }
+}
+
