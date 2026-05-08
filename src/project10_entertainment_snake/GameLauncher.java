0e9459448b08d374fdbf6d9d1fa853c0d92c2629
Noorbi Binte Matharsa <sunshinedaily2288@gmail.com>
2026-05-08 19:46:32 +0200

    Project 10:Snake Game

---
+++ src/project10_entertainment_snake/GameLauncher.java
@@ -0,0 +1,17 @@
+package project10_entertainment_snake;
+
+import javax.swing.JFrame;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.add(new Snake_GUI());
+        frame.setTitle("Project 10: Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+}
+
