c9143de60920acb9ed4a9b062932290a38f2c7ce
Noorbi Binte Matharsa <sunshinedaily2288@gmail.com>
2026-05-10 13:56:32 +0200

    Project 14: Added User/Role logic, password security, and GUI Audit Trail with Save/Clear features

---
+++ src/project14_user_role_management/User.java
@@ -0,0 +1,23 @@
+package project14_user_role_management;
+
+public class User {
+    // 1. Variables (Ingredients)
+    private String username;
+    private Role role;
+    private String password; // Added for security
+
+    // 2. Constructor (The Builder) when this is updated, update all others as well
+    public User(String username, Role role, String password) {
+        this.username = username;
+        this.role = role;
+        this.password = password;
+    }
+
+    // 3. Methods (Getters)
+    public String getUsername() { return username; }
+    public Role getRole() { return role; }
+    public String getPassword() { return password; }
+}
+
+
+
