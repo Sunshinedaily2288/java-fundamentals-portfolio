f2180bba08427bbe18c234b29963087064190787
Noorbi Binte Matharsa <sunshinedaily2288@gmail.com>
2026-05-16 11:14:10 +0200

    feat: complete blog layout and restructure package architecture

--- blog_api_maven/src/main/java/com/matharsa/web_blog/BlogPost.java
+++ blog_api_maven/src/main/java/com/matharsa/web_blog/BlogPost.java
@@ -1,7 +1,17 @@
-
 package com.matharsa.web_blog;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class BlogPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String title;
     private String content;
 
@@ -12,12 +22,15 @@ public class BlogPost {
         this.content = content;
     }
 
+    // Id Getter and Setter
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    // Title Getter and Setter
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
 
+    // Content Getter and Setter
     public String getContent() { return content; }
     public void setContent(String content) { this.content = content; }
 }
-
-
-
